1e484db0b44b218bf4f6c15d45c47020a4ac54b0
toyat522 <toyatakahashi522@gmail.com>
2026-08-02 16:38:58 -0700

    rofi and sway/waybar color theme standardization

--- home/extras.nix
+++ home/extras.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 let
   oriedita = pkgs.callPackage ./pkgs/oriedita.nix { };
@@ -15,14 +15,4 @@ in
     texlive.combined.scheme-medium
     thunderbird
   ];
-
-  # Prevent autostart of fcitx5 by GNOME; only allow systemd service launch
-  xdg.configFile."autostart/org.fcitx.Fcitx5.desktop".text = ''
-    [Desktop Entry]
-    Hidden=true
-  '';
-
-  # Start fcitx5 via systemd with classicui disabled
-  systemd.user.services.fcitx5-daemon.Service.ExecStart = lib.mkForce
-    "${config.i18n.inputMethod.fcitx5.fcitx5-with-addons.override { addons = config.i18n.inputMethod.fcitx5.addons; }}/bin/fcitx5 --disable classicui";
 }
--- home/gnome-full.nix
+++ home/gnome-full.nix
@@ -1,5 +1,17 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [ ./gnome-basic.nix ./extras.nix ];
+
+  # GDM autostarts fcitx5 via the XDG desktop file without --disable classicui, racing the systemd service.
+  # Hide it so GNOME ignores it and only the systemd service (below) launches fcitx5.
+  xdg.configFile."autostart/org.fcitx.Fcitx5.desktop".text = ''
+    [Desktop Entry]
+    Hidden=true
+  '';
+
+  # Under GNOME, graphical-session.target activates the fcitx5 service.
+  # Disable classicui here since GNOME/Wayland draws its own IM UI.
+  systemd.user.services.fcitx5-daemon.Service.ExecStart = lib.mkForce
+    "${config.i18n.inputMethod.fcitx5.fcitx5-with-addons.override { addons = config.i18n.inputMethod.fcitx5.addons; }}/bin/fcitx5 --disable classicui";
 }
